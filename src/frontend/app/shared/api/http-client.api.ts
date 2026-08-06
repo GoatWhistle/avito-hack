1e6191686a79da44d734509928594a470174efb1
FaraneysX <vusioncup@gmail.com>
2026-08-07 01:02:42 +0300

    feat(api): add auth token interceptor to HTTP client

--- src/frontend/app/shared/api/http-client.api.ts
+++ src/frontend/app/shared/api/http-client.api.ts
@@ -9,3 +9,13 @@ const config: CreateAxiosDefaults = {
 }
 
 export const httpClient = axios.create(config)
+
+httpClient.interceptors.request.use(config => {
+  const token = localStorage.getItem('accessToken')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
