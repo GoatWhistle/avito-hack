a4a3b217f75c14975a541516b2031edf814dba13
FaraneysX <vusioncup@gmail.com>
2026-08-08 00:29:47 +0300

    feat(api): add user/me endpoint to API_ENDPOINTS

--- src/frontend/app/shared/api/endpoints.api.ts
+++ src/frontend/app/shared/api/endpoints.api.ts
@@ -1,8 +1,12 @@
 const AUTH = 'auth/'
+const USER = 'users/'
 
 export const API_ENDPOINTS = {
   auth: {
     signUp: `${AUTH}/register`,
     signIn: `${AUTH}/login`,
+  },
+  user: {
+    me: `${USER}/me`,
   },
 } as const
