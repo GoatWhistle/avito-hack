66acb435210028d36045aa8af1c0d9f9cd4094c7
FaraneysX <vusioncup@gmail.com>
2026-08-08 02:23:49 +0300

    feat(lint): configure ESLint flat config with TypeScript, React and Query plugins

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -1,3 +1,33 @@
+import js from '@eslint/js'
 import pluginQuery from '@tanstack/eslint-plugin-query'
+import reactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'typescript-eslint'
+import tseslint from 'typescript-eslint'
 
-export default [...pluginQuery.configs['flat/recommended']]
+export default defineConfig([
+  globalIgnores([
+    '.idea/**',
+    '.react-router/**',
+    'build/**',
+    'node_modules/**',
+    '**/env*',
+    '.gitignore',
+    '.prettierignore',
+    '.prettierrc',
+    'bun.lock',
+    'components.json',
+    'public/**',
+    '**/*.config.js',
+    '**/*.config.ts',
+  ]),
+  js.configs.recommended,
+
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+
+  react.configs.recommended,
+  reactHooks.configs.flat.recommended,
+
+  pluginQuery.configs['flat/recommended'],
+])
